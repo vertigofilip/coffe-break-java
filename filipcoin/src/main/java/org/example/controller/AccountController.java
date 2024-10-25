c37c2192690874cd6776817ada117dd19cddd874
vertigo.filip <44265838+vertigofilip@users.noreply.github.com>
2024-10-25 10:51:13 +0200

    next

--- filipcoin/src/main/java/org/example/controller/AccountController.java
+++ filipcoin/src/main/java/org/example/controller/AccountController.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.controller;
 
 import org.example.account;
 import org.example.service.AccountService;
--- filipcoin/src/main/java/org/example/service/AccountService.java
+++ filipcoin/src/main/java/org/example/service/AccountService.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.service;
 
 import org.example.AccountDao;
 import org.example.account;
